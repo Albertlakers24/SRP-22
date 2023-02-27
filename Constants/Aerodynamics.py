57179c73342a5786adcd4417e00a09eda551b34a
mdinesh <m.dinesh@student.tudelft.nl>
2023-02-27 15:31:41 +0100

    comnstants file demo - aerodynamics

---
+++ Constants/Aerodynamics.py
@@ -0,0 +1,23 @@
+
+CD0_CR =  0.021106443565976893      # CD0 Clean/Cruise [-]
+CD0_15 = 0.04030446169943226        # CD0 Flap deflection 15 deg [-]
+CD0_40 = 0.07520760765686323        # CD0 Flap deflection 40 def [-]
+
+CL_DesCruise = 0.63369832                 # CL Design Cruise [-]
+CD_DesCruise = 0.03158978                 # CD Design Cruise [-]
+CL_CD_DesCruise = 20.06022946             # CL/CD Design Cruise [-]
+Alpha_DesCruise = 4                       # Angle of attack Cruise [deg]
+
+CL_Max_Clean =  1.3499704142011835        # CL max wing clean configuration [-]
+
+
+
+CL_DesLand = 2.1180312426              # CL max 40 deg deflection/1.13 [-]
+CD_DesLand = 0.17010717                # CD at 40 deg flap deflection [-]
+CL_CD_Land = 12.45115767               # CL/CD at 40 deg flap deflection [-]
+Alpha_DesLand = 11.0923                # Alpha at 40 deg flap deflection [-]
+
+CL_Alpha_Wing = 5.63403713             # Lift slope wing [1/rad]
+Cl_Alpha_WingAirfoil =  5.99001331     # Airfoil lift slope [1/rad]
+
+Cl_Alpha_TailAirfoil =  5.7295779513   # Tail Airfoil lift slope [1/rad]
