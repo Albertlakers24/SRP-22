c9eef3e96aab9badb0367916047868584d98aa9b
mdinesh <m.dinesh@student.tudelft.nl>
2023-03-13 14:39:54 +0100

    tail

--- Constants/Empennage_LandingGear.py
+++ Constants/Empennage_LandingGear.py
@@ -1,6 +1,12 @@
 ''' EMPENNAGE & LANDING GEAR OUTPUTS --> INES  ---> Last Update - 27/2/22 '''
 
 # Combined the two because I thought they weren't interrelated and I wanted to make fewer files
+''' TO be updates'''
+
+
+
+t_c_avg_tail = 0.09                     # Average t/c ratio of wing crosssection
+x_c_m_tail = 0.29                        # (x/c)_m Position of max thickness - 0.3 for low speed, 0.5 for high speed
 
 '''To be updated :'''
 lambdahalf_h = 0                # Sweep at half chord of HT     [rad]
@@ -8,11 +14,14 @@ cmac_h = 1                      # MAC for HT                    [m]
 bh = 6                          # Span of the HT                [m]
 c_rh =2                         # HT root chord                 [m]
 c_th =1                         # HT tip chord                  [m]
+c_rv = 3.88                     # VT  root chord                [m]
+c_tv = 1.98                     # VT tip chord                  [m]
 
 ''' Vertical Tail '''
 Sv = 13.96                      # Surface area VT               [m^2]
 taperv = 0.5                    # Taper VT                      [-]
 Vh_V = 1                        # Volume fraction for T-tail    [-]
+#Vh_V = 0.85                    # Volume fraction for conventional tail [-]
 
 ''' Horizontal tail '''
 Sh = 11.376                     # Surface area HT               [m^2]
