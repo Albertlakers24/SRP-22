cd2f87bb6dd92621e3e624f319c6990ae8192f5f
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2023-03-18 18:51:20 +0100

    Week 3 updates

--- Stability_and_Control/Tail_Geometry_Drawings.py
+++ Stability_and_Control/Tail_Geometry_Drawings.py
@@ -102,7 +102,7 @@ def Geometry_elevator(ylower,x_inboard,x_outboard,real,y3,y4,y1,y2,x2,cf_c,crh,c
         ylower_e = ((y3 - y4) / x2) * xlower_e + y4
     return x1_e, x2_e, y1_e, y2_e, y3_e, y4_e, xupper_e, yupper_e, xlower_e, ylower_e,y2_horn
 
-def Plot_Graph(crh, cth,bh,Sweep_LE_HT,Sweep_half_HT,real, colour1,colour2, trimtab, elevator,x_inboard_e,x_outboard_e,x_inboard_t,x_outboard_t,ce_c,cf_c,horn,w_horn,h_horn):
+def Plot_Graph(crh, cth,bh,Sweep_LE_HT,Sweep_half_HT,real, colour1,colour2, trimtab, elevator,x_inboard_e,x_outboard_e,x_inboard_t,x_outboard_t,ce_c,cf_c,horn,w_horn,h_horn,label):
     x1, x2, y1, y2, y3, y4, xupper, yupper, xlower, ylower = Geometry_Drawing(crh,cth,bh,Sweep_LE_HT,Sweep_half_HT,real)
 
     if elevator == 0:
@@ -136,17 +136,26 @@ def Plot_Graph(crh, cth,bh,Sweep_LE_HT,Sweep_half_HT,real, colour1,colour2, trim
         plt.vlines(x=x2_e, ymin=y3_e, ymax=y2_e, color=colour2)
         plt.vlines(x=x1_e, ymin=y4_e, ymax=y1_e, color=colour2)
 
-    plt.plot(xupper, yupper, color=colour1, alpha=0.4)
-    plt.plot(xlower, ylower, color=colour1, alpha=0.4)
-    plt.vlines(x=x2, ymin=y3, ymax=y2, color=colour1, alpha=0.4)
-    plt.vlines(x=x1, ymin=y4, ymax=y1, color=colour1, alpha=0.4)
-    plt.fill_between(xlower, ylower, yupper, color=colour2, alpha=0.1)
+    plt.plot(xupper, yupper, color=colour2, alpha=1)
+    plt.plot(xlower, ylower, color=colour2, alpha=1)
+    plt.vlines(x=x2, ymin=y3, ymax=y2, color=colour2, alpha=1)
+    plt.vlines(x=x1, ymin=y4, ymax=y1, color=colour2, alpha=1)
+    plt.fill_between(xlower, ylower, yupper, color=colour2, alpha=0.1,label=label)
+    plt.legend(loc="upper left")
     return
 
-# 1st Fidelity Initial Sizing
-Plot_Graph(crh=2.0464, cth=1.53,bh=7.16,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=10*np.pi/180,real=1, colour1="#728FCE", colour2="#98AFC7", trimtab=1,elevator=1,x_inboard_e=1,x_outboard_e=6,x_inboard_t=2,x_outboard_t=5,ce_c=0.3,cf_c=0.1,horn=0,w_horn=0.4,h_horn=0.8)
+# 1st Fidelity; Initial Sizing
+Plot_Graph(crh=2.0464, cth=1.53,bh=7.3,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=5*np.pi/180,real=1, colour1="#728FCE", colour2="#98AFC7", trimtab=1,elevator=1,x_inboard_e=1,x_outboard_e=6,x_inboard_t=2,x_outboard_t=5,ce_c=0.3,cf_c=0.1,horn=1,w_horn=0.4,h_horn=0.8, label="1st Fidelity")
 
+# 2nd Fidelity; Second Sizing (elevator)
+Plot_Graph(crh=1.8,cth=1.3,bh=7.16,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=5*np.pi/180,real=1, colour1="#FA8072", colour2="#CD5C5C", trimtab=1,elevator=0,x_inboard_e=1,x_outboard_e=3.58,x_inboard_t=1.5,x_outboard_t=5,ce_c=0.3,cf_c=0.1,horn=1,w_horn=0.4,h_horn=0.8,label="2nd Fidelity")
+
+# 3rd Fidelity; Third Sizing (trimtab)
+Plot_Graph(crh=1.8,cth=1.3,bh=7.16,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=10*np.pi/180,real=1, colour1="#FF5733", colour2="#F99781", trimtab=0,elevator=0,x_inboard_e=1,x_outboard_e=3.58,x_inboard_t=1.5,x_outboard_t=2.8,ce_c=0.3,cf_c=0.1,horn=1,w_horn=0.4,h_horn=0.8,label="3rd Fidelity")
+
+# 4th Fidelity; Fourth Sizing (horn)
+#Plot_Graph(crh=1.8,cth=1.3,bh=7.16,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=10*np.pi/180,real=1, colour1="#C863F8", colour2="#D796F7", trimtab=0,elevator=0,x_inboard_e=1,x_outboard_e=3.58,x_inboard_t=1.5,x_outboard_t=2.8,ce_c=0.3,cf_c=0.1,horn=0,w_horn=0.4,h_horn=0.8,label="4th Fidelity")
 
 # Final Design
-Plot_Graph(crh=4, cth=3,bh=12,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=10*np.pi/180,real=0,colour1="#11aa00",colour2="#11aa0055", trimtab=0,elevator=0,x_inboard_e=-1,x_outboard_e=-6,x_inboard_t=-2,x_outboard_t=-5,ce_c=0.3,cf_c=0.1, horn=0,w_horn=0.4,h_horn=0.8)
+Plot_Graph(crh=1.6, cth=1.3,bh=7.16,Sweep_LE_HT=10*np.pi/180,Sweep_half_HT=10*np.pi/180,real=0,colour1="#11aa00",colour2="#11aa0055", trimtab=0,elevator=0,x_inboard_e=-1,x_outboard_e=-3.58,x_inboard_t=-1.5,x_outboard_t=-3.2,ce_c=0.3,cf_c=0.1, horn=0,w_horn=0.3,h_horn=0.4,label="Final Design")
 plt.show()
