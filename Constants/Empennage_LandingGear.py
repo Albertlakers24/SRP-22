dfc78e8305918cec8134cfbba28c1508bc2c48fb
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2023-04-03 14:16:49 +0200

    Monday updates

--- Constants/Empennage_LandingGear.py
+++ Constants/Empennage_LandingGear.py
@@ -1,37 +1,54 @@
 ''' EMPENNAGE & LANDING GEAR OUTPUTS --> INES  ---> Last Update - 13/3/22 '''
 import numpy as np
-# Combined the two because I thought they weren't interrelated and I wanted to make fewer files
 
-tc_tail = 0.09                     # Average t/c ratio of wing crosssection
-xc_m_tail = 0.29                        # (x/c)_m Position of max thickness - 0.3 for low speed, 0.5 for high speed
+'''To be updated:'''
+Av_eff = 3.5                    # VT: effective Aspect Ratio    [-]         FILE: Sizing_Vertical_Tail
+xc_m_tail = 0.29                # (x/c)_m Position of max thickness - 0.3 for low speed, 0.5 for high speed
 
-'''To be updated :'''
-Sweep_quarter_chord_H = 0       # HT c/4 sweep angle            [rad]
-Sweep_halfchord_VT = 0          # VT c/2 sweep angle            [rad]
-lambdahalf_h = 0                # Sweep at half chord of HT     [rad]
-cmac_h =  1.614 #1              # MAC for HT                    [m]
-bh = 6                          # Span of the HT                [m]
-c_rh = 1.98 #2                  # HT root chord                 [m]
-c_th =  1.29 #1                 # HT tip chord                  [m]
-cmac_v =  3.204                 # MAC for VT                    [m]
-c_rv = 3.88                     # VT  root chord                [m]
-c_tv = 1.98                     # VT tip chord                  [m]
-lv = 10.2                       # VT tail arm                   [m]
-Av_eff = 3.5                    # VT : effective Aspect Ratio   [-]         FILE: Sizing_Vertical_Tail
-
-''' Vertical Tail '''
-Sv = 13.96                      # Surface area VT               [m^2]
-taperv = 0.5                    # Taper VT                      [-]
+''''First Fidelity: TAILARM'''
 Vh_V = 1                        # Volume fraction for T-tail    [-]
-Av = 4
-#Vh_V = np.sqrt(0.85)                    # Volume fraction for conventional tail [-]
-
-''' Horizontal tail '''
-Sh = 11.376                     # Surface area HT               [m^2]
-taperh = 0.75                   # Taper HT                      [-]
-x_h = 23.3                      # x-location horizontal tail    [m]
-A_h = 8                         # HT aspect ratio               [-]
-lh = 10.6                       # Tail arm                      [m]
-
+Sh = 15.506                     # HT: Surface area              [m^2]
+taperh = 0.75                   # HT: Taper                     [-]
+x_h = 21.45                     # HT: xlocation from nose to ac [m]
+A_h = 4                         # HT: aspect ratio              [-]
+lh = 8.85                       # HT: Tail arm                  [m]
+Sv = 19.33                      # VT: Surface area              [m^2]
+taperv = 0.5                    # VT: Taper                     [-]
+Av = 1.3                        # VT: Aspect ratio              [-]
+tc_tail_vt = 0.18               # VT: t/c ratio                 [-]
+Sweep_quarter_chord_H = 0       # HT: c/4 sweep angle           [rad]
+Sweep_halfchord_VT = 0          # VT: c/2 sweep angle           [rad]
+lambdahalf_h = 0                # HT: c/2 sweep angle           [rad]
+cmac_h =  1.982                 # HT: MAC                       [m]
+bh = 7.8756                     # HT: Span                      [m]
+c_rh = 2.2502                   # HT: root chord                [m]
+c_th =  1.6876                  # HT: tip chord                 [m]
+cmac_v =  3.817                 # VT: MAC                       [m]
+c_rv = 4.908                    # VT: root chord                [m]
+c_tv = 2.45                     # VT: tip chord                 [m]
+lv = 8.30                       # VT: tail arm                  [m]
 
 ''' Landing Gear '''
+
+'''Old Values'''
+# Sweep_quarter_chord_H = 0       # HT c/4 sweep angle            [rad]
+# Sweep_halfchord_VT = 0          # VT c/2 sweep angle            [rad]
+# lambdahalf_h = 0                # Sweep at half chord of HT     [rad]
+# cmac_h =  1.614 #1              # MAC for HT                    [m]
+# bh = 6                          # Span of the HT                [m]
+# c_rh = 1.98 #2                  # HT root chord                 [m]
+# c_th =  1.29 #1                 # HT tip chord                  [m]
+# cmac_v =  3.204                 # MAC for VT                    [m]
+# c_rv = 3.88                     # VT  root chord                [m]
+# c_tv = 1.98                     # VT tip chord                  [m]
+# lv = 10.2                       # VT tail arm                   [m]
+# Av_eff = 3.5                    # VT : effective Aspect Ratio   [-]         FILE: Sizing_Vertical_Tail
+# Sv = 13.96                      # Surface area VT               [m^2]
+# taperv = 0.5                    # Taper VT                      [-]
+# Vh_V = 1                        # Volume fraction for T-tail    [-]
+# Av = 4                          # VT aspect ratio               [-]
+# Sh = 11.376                     # Surface area HT               [m^2]
+# taperh = 0.75                   # Taper HT                      [-]
+# x_h = 23.3                      # x-location horizontal tail    [m]
+# A_h = 8                         # HT aspect ratio               [-]
+# lh = 10.6                       # Tail arm                      [m]
--- Stability_and_Control/Initial_Tail_Sizing.py
+++ Stability_and_Control/Initial_Tail_Sizing.py
@@ -7,8 +7,8 @@ print("FILE: Initial Tail Sizing")
 Initial = True
 
 # Inputs: Esimated Variables
-x_h = 23.3                      # Location horizontal tail  [m]
-x_v = 21.9                      # Location vertical tail    [m]
+x_h = 21.45                     # HT: nose to ac of HT      [m]
+x_v = 20.9                      # VT: nose to ac of VT      [m]
 Vh = 0.86                       # Horizontal tail volume    [m^3]   -> 0.68113 (propeller)
 Vv = 0.08                       # Vertical tail volume      [m^3]   -> 0.08    (expected for propeller)
 Av = 1.3                        # VT aspect ratio           [-]     -> 1-2
@@ -19,6 +19,7 @@ Kc = 1.4
 Sh_newvalue = 11.376            # HT surface area           [m^2]
 Sv_newvalue = 13.96             # VT surface area           [m^2]
 Sweepquarterchord_h = 0         # HT c/4 sweep              [rad]
+Sweep_LE_vt = 30*np.pi/180       # VT LE sweep               [rad]
 
 # Intermediate Calculations
 l_v = x_v - xcg_aft_potato      # VT location               [m]
@@ -26,8 +27,10 @@ l_h = x_h - xcg_aft_potato      # HT location               [m]
 l_opt = Kc * np.sqrt((4 * c_mac_w * S_w * Vh) / (np.pi * d_f_outer))
 
 print("-----------Location check---------")
-print("l_v =", l_v)
-print("l_opt=", l_opt)
+print("x_v =", x_v)
+print("x_h =", x_h)
+print("l_v =",l_v)
+print("l_h = ", l_h)
 
 if Initial == True:
     Sh = Vh * (S_w * c_mac_w) / l_h
@@ -44,7 +47,8 @@ def Geometry_HT():
     t_c_ratioh = 0.18
     c_mach_h = (2 / 3) * c_rh * ((1 + taperh + taperh ** 2) / (1 + taperh))
     y_mach_h = 0.5 * (1 / 3) * (1 + 2 * taperh) / (1 + taperh) * bh
-    return bh, c_rh,c_th,c_mach_h,y_mach_h
+    d_LE_h = x_h - c_mach_h/4                                               # Distance nose a/c to LE HT
+    return bh, c_rh,c_th,c_mach_h,y_mach_h,d_LE_h
 
 def Geometry_VT():
     bv = np.sqrt(Av * Sv)
@@ -53,9 +57,11 @@ def Geometry_VT():
     c_tv = taperv * c_rv
     c_mac_v = (2 / 3) * c_rv * ((1 + taperv + taperv ** 2) / (taperv + 1))
     y_mach_v = 0.5 * (1 / 3) * (1 + 2 * taperv) / (1 + taperv) * bv
-    return bv,c_rv,c_tv,c_mac_v,y_mach_v
+    d_LE_v = x_v - c_mac_v/4 - y_mach_v*np.tan(Sweep_LE_vt)                     # Distance nose a/c to LE VT
+    return bv,c_rv,c_tv,c_mac_v,y_mach_v,d_LE_v
 
 print("-----------Horizontal Tail Sizing------------")
+print("d_LE_h =", Geometry_HT()[5], "m")
 print("Sh =", Sh, "m^2")
 print("Ah =", Ah)
 print("bh =", Geometry_HT()[0], "m")
@@ -67,6 +73,7 @@ print("ratio wing areas =", Sh / S_w)
 print("tail_arm_h=", x_h - xcg_aft_potato, "m")
 
 print("-----------Vertical Tail Sizing------------")
+print("d_LE_v =", Geometry_VT()[5], "m")
 print("Sv =", Sv, "m^2")
 print("Av =", Av)
 print("bv =", Geometry_VT()[0], "m")
