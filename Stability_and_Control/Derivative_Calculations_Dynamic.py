236fbe42db20fbe170aeb04a634168ed64834277
ElisabethInes <74304402+ElisabethInes@users.noreply.github.com>
2023-05-11 00:00:21 +0200

    Monday updates

--- Stability_and_Control/Derivative_Calculations_Dynamic.py
+++ Stability_and_Control/Derivative_Calculations_Dynamic.py
@@ -1,6 +1,6 @@
 import numpy as np
 import math as m
-from Constants.MissionInputs import rho_0, g, V_cruise, phi, Psi
+from Constants.MissionInputs import rho_0, g, V_cruise, phi, Psi,rho_cruise
 from Constants.Masses_Locations import m_mto, xcg_aft_potato, LEMAC
 from Constants.AircraftGeometry import S_w, Aw, c_mac_w, bw
 from Constants.Aerodynamics import CL_DesCruise, CL_Alpha_Wing, downwash, CL_Alpha_HT
@@ -10,18 +10,21 @@ from Constants.Stability_Control import CNh_delta, Chdelta, Chalpha, CNhalpha_fr
 print("FILE: Derivative_Calculations_Dynamic")
 gamma0 =0                               # Steady horizontal flight FD p163
 
-Oswald = 1/((np.pi)*Aw*Psi+(1/phi))      #-      Oswald Efficiency Factor
+Oswald = 1/((np.pi)*Aw*Psi+(1/phi))     #-      Oswald Efficiency Factor
+DeltaOswald_TO = 0.0026*15              #-      Change in Oswald Efficiency Factor at TO, with 15degrees flap deflection
+print("Oswald =", Oswald)
+print("Oswald_TO=", DeltaOswald_TO+Oswald)
 
 def Zero_Derivatives():
     """Derivatives in Steady Flight
     :param W; weight [N]
     :param theta0; [rad]"""
-    CX0 = m_mto*g * np.sin(gamma0) / (0.5 * rho_0 * V_cruise ** 2 * S_w)
-    CZ0 =-m_mto*g * np.cos(gamma0) / (0.5 * rho_0 * V_cruise ** 2 * S_w)
+    CX0 = m_mto*g * np.sin(gamma0) / (0.5 * rho_cruise * V_cruise ** 2 * S_w)
+    CZ0 =-m_mto*g * np.cos(gamma0) / (0.5 * rho_cruise * V_cruise ** 2 * S_w)
     Cm0 = 0
     return CX0, CZ0, Cm0
 
-def Velocity_Derivatives(): #CHECKED
+def Velocity_Derivatives():
     CXu = 2*CL_DesCruise* np.tan(gamma0)
     CZu = -2* CL_DesCruise
     Cmu = 0
@@ -29,7 +32,7 @@ def Velocity_Derivatives(): #CHECKED
 
 def Attack_Derivative(): #CHECKED
     CXalpha = CL_DesCruise*(1-((2*CL_Alpha_Wing)/(np.pi*Aw*Oswald)))
-    CZalpha = -CL_Alpha_Wing
+    CZalpha = -CL_Alpha_Wing-CNhalpha_free*(1-downwash)*Vh_V**2*(Sh/S_w)
     # Cmalpha = (CL_Alpha_Wing *(x_cg - x_w)/c_mac) - CL_Alpha_HT*(1-downwash)*Vh_V**2*Sh*l_h/Sw/c_mac
     # Cmalpha1 = CL_Alpha_Wing * (x_cg - x_w)/c_mac + -((CL_Alpha_HT *(1-downwash))+(0.024*180/np.pi)*-0.8741417)*((Sh*l_h)/(Sw*c_mac))
     x_w = LEMAC + 0.25 * c_mac_w
@@ -40,13 +43,12 @@ def Attack_Derivative(): #CHECKED
     CXalpha_dot = 0                                                                         #unknown
     return CXalpha, CZalpha, Cmalpha, CZalpha_dot, Cmalpha_dot, CXalpha_dot #, Cmalpha1, Cmalpha2
 
-
 def Pitch_Derivative(): #CHECKED
     CXq =0
     CZq = -2*CL_Alpha_HT*(Vh_V**2)*(Sh*lh/(S_w*c_mac_w))
-    Cmq = -1.1 * CL_Alpha_HT * Vh_V ** 2 * Sh * lh**2 / (S_w * c_mac_w**2)
+    Cmq = -1.4* CL_Alpha_HT * Vh_V ** 2 * Sh * lh**2 / (S_w * c_mac_w**2)
     return CXq, CZq,Cmq
-
+print("Cmq=", Pitch_Derivative()[2])
 def Delta_e_Derivatives(): #CHECKED
     CXde = 0
     CZde = - CNh_delta *Vh_V**2*Sh/S_w
